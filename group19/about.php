d4b0568d1a5e02604d9477303e4aaad093780da6
mylavencill <mbvenc@colostate.edu>
2026-04-06 13:52:57 -0600

    Initializing the About Page layout

---
+++ group19/about.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>About Page</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="./index.php">Homepage</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="./color.php">Color Coordinate Page</a></li>
+            </ul>
+        </nav>
+
+        <header>
+            <h1>About Page</h1>
+        </header>
+
+            <div>
+                <!-- Anna's About -->
+            </div>
+
+            <div>
+                <!-- Jennifer's About -->
+            </div>
+
+            <div>
+                <!-- Myla's About -->
+            </div>
+
+            <div>
+                <!-- Jim's About -->
+            </div>
+
+        <footer>
+
+        </footer>
+    </body>
+</html>
