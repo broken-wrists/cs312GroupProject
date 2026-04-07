75d0c9ab5754e85689c6716a5078d0ad10a7fba6
broken-wrists <compsci2233@gmail.com>
2026-04-07 16:24:16 -0600

    4.1 input form

--- group19/color.php
+++ group19/color.php
@@ -8,8 +8,32 @@
         <title>Color Coodinate Page</title>
     </head>
     <body>
+
         <header>
+        <nav>
+           <a href="index.php">Home</a> |
+           <a href="about.php">About</a> |
+           <a href="color.php">Color</a> 
+        </nav>
+
+        <h1> Color Coordinator</h1>
+
         </header>
+
+        <form method = "post" action = "color.php">
+            <label for = "gridsize"> Rows and Columns (1-26):</label>
+            <input type = "number" name = "grid_size" id="gridsize" min="1" max="26">
+            <br><br>
+
+            <label for = "num_colors"> Number Of Colors (1-10): </label>
+            <input type="number" name="numColors" id="numColors" min="1" max="10">
+            <br><br>
+
+            <button type = "submit">Generate</button>
+
+        </form>
+
+
         <footer>
         </footer>
     </body>
