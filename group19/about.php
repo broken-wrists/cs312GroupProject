cbb6a30a08e401b0d279b12f934503ee50121888
jenpyle <jennypyle440@gmail.com>
2026-04-07 14:32:27 -0600

    Add Jennifer about and picture

--- group19/about.php
+++ group19/about.php
@@ -1,44 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>About Page</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <nav>
-            <ul>
-                <li><a href="./index.php">Homepage</a></li>
-                <li><a href="#">About Me</a></li>
-                <li><a href="./color.php">Color Coordinate Page</a></li>
-            </ul>
-        </nav>
 
-        <header>
-            <h1>About Page</h1>
-        </header>
+<head>
+    <meta charset="utf-8">
+    <title>About Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 
-            <div>
-                <h2>Anna</h2>
-                <p>Senior in computer science with a minor in mathematics at CSU. A few of my hobbies include snowboarding, gaming, dnd, and boating.</p>
-            </div>
+<body>
+    <nav>
+        <ul>
+            <li><a href="./index.php">Homepage</a></li>
+            <li><a href="#">About Me</a></li>
+            <li><a href="./color.php">Color Coordinate Page</a></li>
+        </ul>
+    </nav>
 
-            <div>
-                <!-- Jennifer's About -->
-            </div>
+    <header>
+        <h1>About Page</h1>
+    </header>
 
-            <div>
-                <!-- Myla's About -->
-            </div>
+    <div>
+        <h2>Anna</h2>
+        <p>Senior in computer science with a minor in mathematics at CSU. A few of my hobbies include snowboarding,
+            gaming, dnd, and boating.</p>
+    </div>
 
-            <div>
-                <!-- Jim's About -->
-                 <h2>Jim</h2>
-                 <p>Computer Science student at CSU with a passion for all things tech. Member of Devnet, Hashdump and OpenOrbit clubs at CSU.</p>
-            </div>
+    <div>
+        <h2>Jennifer</h2>
+        <p>I am from Colorado and study computer science at CSU. In my free time, I enjoy: rock climbing, camping, and
+            hanging out with my cats</p>
+    </div>
 
-        <footer>
+    <div>
+        <!-- Myla's About -->
+    </div>
 
-        </footer>
-    </body>
+    <div>
+        <!-- Jim's About -->
+        <h2>Jim</h2>
+        <p>Computer Science student at CSU with a passion for all things tech. Member of Devnet, Hashdump and OpenOrbit
+            clubs at CSU.</p>
+    </div>
+
+    <footer>
+
+    </footer>
+</body>
+
 </html>
